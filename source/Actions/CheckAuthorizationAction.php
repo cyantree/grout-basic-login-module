f86be11a0f9b02c1fdf8fabba3aa4d7c1fe176c8
cyantree <mail@cyantree.com>
2014-08-21 21:54:39 +0200

    Authorization: Reworked authorization management. Cookie will now be saved with path "/" to be valid for whole application

--- source/Actions/CheckAuthorizationAction.php
+++ source/Actions/CheckAuthorizationAction.php
@@ -19,38 +19,55 @@ class CheckAuthorizationAction
     {
         if($this->username === null || $this->password === null){
             return false;
+
         }else{
+            $setCookie = false;
             $cookieName = 'grout_login_' . substr(md5($this->name . $this->username), 0, 8);
-
-            // Check authorization cookie
-            $data = explode('_', $this->task->request->cookies->get($cookieName));
-            if (count($data) == 2) {
-                if (time() - $data[0] < $this->expiration) {
-                    if (md5($data[0] . $this->username . $this->password . $this->name) == $data[1]) {
-
-                        if ($this->extendExpiration) {
-                            $t = time();
-                            setcookie($cookieName, $t . '_' . md5($t . $this->username . $this->password . $this->name), $t + $this->expiration, null, null, null, true);
-                        }
-                        return true;
-                    }
-                }
-            }
-
+            $success = false;
 
             // Check post
             if ($this->task->request->post->get('login')) {
-                if ($this->task->request->post->get('username') == $this->username &&
-                      $this->task->request->post->get('password') == $this->password) {
+                if ($this->task->request->post->get('username') != $this->username ||
+                      $this->task->request->post->get('password') != $this->password) {
+                    return false;
+                }
 
-                    $t = time();
-                    setcookie($cookieName, $t . '_' . md5($t . $this->username . $this->password . $this->name), $t + $this->expiration, null, null, null, true);
+                $setCookie = true;
+                $success = true;
+            }
 
-                    return true;
+            // Check authorization cookie
+            if (!$success) {
+                $data = explode('_', $this->task->request->cookies->get($cookieName));
+
+                if (count($data) == 2) {
+                    if (time() - $data[0] > $this->expiration) {
+                        return false;
+                    }
+
+                    if (md5($data[0] . $this->username . $this->password . $this->name) != $data[1]) {
+                        return false;
+                    }
+
+                    if ($this->extendExpiration) {
+                        $setCookie = true;
+                    }
+
+                    $success = true;
                 }
             }
-        }
 
-        return false;
+            if ($success) {
+                if ($setCookie) {
+                    $t = time();
+                    setcookie($cookieName, $t . '_' . md5($t . $this->username . $this->password . $this->name), $t + $this->expiration, '/', null, null, true);
+                }
+
+                return true;
+
+            } else {
+                return false;
+            }
+        }
     }
 }
